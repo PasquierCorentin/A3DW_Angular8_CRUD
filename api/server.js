c330accfecd406cec63134bf2582b8076e102185
CorentinPasquier78 <corentin.pasquier@edu.devinci.fr>
2019-09-20 08:18:52 +0200

    Back finish + data display

--- api/server.js
+++ api/server.js
@@ -1,3 +1,4 @@
+
 const express = require('express'),
   path = require('path'),
   bodyParser = require('body-parser'),
@@ -5,6 +6,7 @@ const express = require('express'),
   mongoose = require('mongoose'),
   config = require('./DB');
 
+const productRoute = require('./routes/product.route');
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
   () => {console.log('Database is connected') },
@@ -14,6 +16,7 @@ mongoose.connect(config.DB, { useNewUrlParser: true }).then(
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use('/products', productRoute);
 const port = process.env.PORT || 4000;
 
 const server = app.listen(port, function(){
